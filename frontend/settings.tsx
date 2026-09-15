646945fe406a3b640ebfef4e22715deb41e3754e
SaiyajinK <105972098+SaiyajinK@users.noreply.github.com>
2026-09-15 17:44:19 +0200

    Update settings.tsx

--- frontend/settings.tsx
+++ frontend/settings.tsx
@@ -1,4 +1,4 @@
-import { Field, Toggle } from "@steambrew/client";
+import { Field, Toggle } from "@steambrew/client";s
 import { useEffect, useState } from "react";
 import { getLanguageKey, TEXT } from "./i18n";
 
